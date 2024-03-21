fbfffcfaa4d5ddd8019c05615efcf32546f73356
João Elias Ferraz Santana <65469472+eng-joaoelias@users.noreply.github.com>
2024-03-21 20:57:34 -0300

    Update ex002.py

--- exercicios_210324/ex002.py
+++ exercicios_210324/ex002.py
@@ -1,8 +1,11 @@
 def fibonacci(qtdTermos):
+    x = 0
     if qtdTermos == 1:
-        return 1
+        x = 1
+        return x
     if qtdTermos == 2:
-        return 1
+        x = 1
+        return x
     x = fibonacci(qtdTermos - 1) + fibonacci(qtdTermos - 2)
     print(x)
     return x
