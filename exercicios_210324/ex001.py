e6b578716acdfd188d68df56ae6f563f63ee8571
João Elias Ferraz Santana <65469472+eng-joaoelias@users.noreply.github.com>
2024-03-22 10:20:20 -0300

    Update ex001.py
    Ajuste no problema: agora, o programa verifica se os valores estatísticos aumentaram ou diminuíram.

--- exercicios_210324/ex001.py
+++ exercicios_210324/ex001.py
@@ -1,36 +1,34 @@
-listaTmeperaturas = [19.5, 19.5, 21.6, 20.2, 19.7, 20.2, 18.6, 17.2, 19.5]
-
-def calculosEstatisticos():
-    listaTmeperaturas.sort()
+def calculosEstatisticos(listaTemperaturas):
+    listaTemperaturas.sort()
 
     acum = 0
 
-    for temperatura in listaTmeperaturas:
+    for temperatura in listaTemperaturas:
         acum = acum + temperatura
         
-    media = acum / len(listaTmeperaturas)
+    media = acum / len(listaTemperaturas)
 
     mediana = 0
 
-    print(f'O conjunto informado é {listaTmeperaturas}')
+    print(f'O conjunto informado é {listaTemperaturas}')
 
-    if len(listaTmeperaturas) % 2 == 0:
-        pos1 = int(len(listaTmeperaturas) / (2) - 1)
+    if len(listaTemperaturas) % 2 == 0:
+        pos1 = int(len(listaTemperaturas) / 2 - 1)
         pos2 = pos1 + 1
-        mediana = (listaTmeperaturas[pos1] + listaTmeperaturas[pos2]) / 2
+        mediana = (listaTemperaturas[pos1] + listaTemperaturas[pos2]) / 2
     else:
-        pos = int((len(listaTmeperaturas) + 1) / (2) - 1)
-        mediana = listaTmeperaturas[pos]
+        pos = int((len(listaTemperaturas) + 1) / 2 - 1)
+        mediana = listaTemperaturas[pos]
         
     contagem = {}
 
-    for elemento in listaTmeperaturas:
+    for elemento in listaTemperaturas:
         if elemento in contagem:
-            contagem[elemento] = contagem[elemento] + 1
+            contagem[elemento] += 1
         else:
             contagem[elemento] = 1
             
-    moda = max(contagem, key = contagem.get)
+    moda = max(contagem, key=contagem.get)
 
     print(f'A média é {media}')
     print(f'A mediana é {mediana}')
@@ -38,16 +36,55 @@ def calculosEstatisticos():
 
     numeradorVariancia = 0
 
-    for elemento in listaTmeperaturas:
-        numeradorVariancia = numeradorVariancia + ((elemento - media)**2)
-    variancia = numeradorVariancia / len(listaTmeperaturas)
+    for elemento in listaTemperaturas:
+        numeradorVariancia += (elemento - media) ** 2
+    variancia = numeradorVariancia / len(listaTemperaturas)
     
     print(f'A variância é {variancia}')
 
     desvioPad = variancia ** (0.5)
     
     print(f'O desvio padrão é {desvioPad}')
+    valoresEstatistica = []
+    valoresEstatistica.append(media)
+    valoresEstatistica.append(mediana)
+    valoresEstatistica.append(moda)
+    valoresEstatistica.append(variancia)
+    valoresEstatistica.append(desvioPad)
+    return valoresEstatistica
     
-calculosEstatisticos()
-listaTmeperaturas.append(20.2)
-calculosEstatisticos()
+def main():
+    
+    listaTemperaturas = [19.5, 19.5, 21.6, 20.2, 19.7, 20.2, 18.6, 17.2, 19.5]
+
+    lista1 = calculosEstatisticos(listaTemperaturas)
+    listaTemperaturas.append(20.2)
+    lista2 = calculosEstatisticos(listaTemperaturas)
+    
+    if lista1[0] < lista2[0]:
+        print('A média aumentou!')
+    else:
+        print('Diminuição da média')
+        
+    if lista1[1] < lista2[1]:
+        print('A mediana aumentou!')
+    else:
+        print('Diminuição da mediana')
+        
+    if lista1[2] < lista2[2]:
+        print('A moda aumentou!')
+    else:
+        print('Diminuição da moda')
+        
+    if lista1[3] < lista2[3]:
+        print('A variância aumentou!')
+    else:
+        print('Diminuição da variância')
+        
+    if lista1[4] < lista2[4]:
+        print('O desvio padrão aumentou!')
+    else:
+        print('Diminuição do desvio padrão')
+    
+if __name__ == "__main__":
+    main()
